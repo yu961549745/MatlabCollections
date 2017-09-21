711357b606b4fbc4d186e26d7d9b49f41d3f5696
yjt <961549745@qq.com>
2017-09-21 21:01:10 +0800

    修改成通用调用方式

--- ATSP/wolframTSP.m
+++ ATSP/wolframTSP.m
@@ -1,6 +1,6 @@
 function [x,f] = wolframTSP(d)
 save graph.txt d -ascii -double
-[~,cmdout]=system('wolfram -script tsp.wl');
+[~,cmdout]=system('wolframscript -file tsp.wl');
 y=str2double(regexp(cmdout,'\d+(\.\d+)?','match'));
 x=y(2:end-1);
 f=y(1);
