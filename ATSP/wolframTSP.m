fe61762a8e318583e0d7099552c0f308d452bd15
yjt <961549745@qq.com>
2017-09-25 19:21:26 +0800

    更新TSP

--- ATSP/wolframTSP.m
+++ ATSP/wolframTSP.m
@@ -1,6 +1,10 @@
 function [x,f] = wolframTSP(d)
 save graph.txt d -ascii -double
-[~,cmdout]=system('wolframscript -file tsp.wl');
+if ispc
+    [~,cmdout]=system('wolfram -script tsp.wl');
+else
+    [~,cmdout]=system('wolframscript -file tsp.wl');
+end
 y=str2double(regexp(cmdout,'\d+(\.\d+)?','match'));
 x=y(2:end-1);
 f=y(1);
