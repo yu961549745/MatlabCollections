7a90d86c38bd193510620a2ab1ad264e707f7bcf
yjt <961549745@qq.com>
2017-12-19 15:42:59 +0800

    用MATLAB自带函数实现可以指定编码的全文读取

--- TextReader/readtext.m
+++ TextReader/readtext.m
@@ -1,4 +1,12 @@
-function x=readtext(fname,encode)
-javaaddpath('JTXT.jar');
-x=char(jtxt.Reader.read(fname,encode));
+function t = readtext(f,encoding)
+if nargin<2
+    opts={};
+else
+    opts={'n',encoding};
+end
+fid=fopen(f,'r',opts{:});
+t=textscan(fid,'%[^\n]');
+fclose(fid);
+t=t{1};
+t=sprintf('%s\n',t{:});
 end
