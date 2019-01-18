69be270d764911b3278d6a18a130a10d1b561f57
yjt <961549745@qq.com>
2019-01-18 16:32:15 +0800

    修正溢出BUG

--- ExcelColNum/mex_excel_col.cpp
+++ ExcelColNum/mex_excel_col.cpp
@@ -3,24 +3,17 @@
 void toAZ(std::vector<char>& v, int x) {
 	while (x > 0) {
 		int rem = x % 26;
-		if (rem == 0) {
-			v.push_back('Z');
-			x = x / 26 - 1;
-		}
-		else {
-			v.push_back(rem - 1 + 'A');
-			x /= 26;
-		}
+		if (rem == 0) rem = 26;
+		v.push_back(rem - 1 + 'A');
+		x = (x - rem) / 26;
 	}
 }
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	std::vector<char> v;
 	toAZ(v, (int)(mxGetScalar(prhs[0])));
-	char* str = new char[v.size()];
+	char* str = new char[v.size() + 1];
 	int k = 0;
-	for (auto i = v.rbegin(); i != v.rend(); i++) {
-		str[k++] = *i;
-	}
+	for (auto i = v.rbegin(); i != v.rend(); i++) str[k++] = *i;
 	str[k] = '\0';
 	plhs[0] = mxCreateString(str);
 	delete str;
