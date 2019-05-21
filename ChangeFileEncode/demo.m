dbc878755139ff52ab0241adb1fe332b39c18d86
yjt <yu_jiang_tao@qq.com>
2019-05-21 22:59:50 +0800

    修改编码

--- ChangeFileEncode/demo.m
+++ ChangeFileEncode/demo.m
@@ -1,3 +1,2 @@
 clc,clear,close all;
-javaaddpath('lib.jar');
-yjt.ChangeFileEncode.convert('./fs/1.csv','windows-1252','./fs/2.csv','utf8');
+yjt.ChangeFileEncode.convert('fs/1.csv','windows-1252','fs/2.csv','utf8');
