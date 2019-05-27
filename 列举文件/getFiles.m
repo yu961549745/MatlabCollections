5d5cf4a7042653e97d70dfa6b38dca000e52b89e
yjt <961549745@qq.com>
2019-05-27 21:48:36 +0800

    列举文件支持默认通配符

--- 列举文件/getFiles.m
+++ 列举文件/getFiles.m
@@ -4,29 +4,28 @@ function fs = getFiles(path,reg,exReg)
 if nargin<3
     % 默认无视 windows 下的 db 文件
     % 和 mac 下的 . 开头的索引文件
-    exReg='(.*\.db)|(^\..*)';
+    % 以及 . 和 .. 
+    exReg='(.*\.db)|(^\.{1,2}.*)';
     if nargin<2
         reg='';
     end
 end
 % 按文件名筛选
-sfs=dir(path);
-fs=arrayfun(@(f){f.name},sfs);
-fds=arrayfun(@(f){f.folder},sfs);
-dfs=arrayfun(@(f)f.isdir,sfs);
-fs=fs(~dfs);
-fds=fds(~dfs);
+fs=dir(path);
+if isempty(fs)
+    error('no file found.');
+end
 if ~isempty(reg)
-    ind=cellfun(@(s)~isempty(regexp(s,reg,'once')),fs);
+    ind=arrayfun(@(s)~isempty(regexp(s.name,reg,'once')),fs);
 else
     ind=true(size(fs));
 end
 if ~isempty(exReg)
-    exInd=cellfun(@(s)~isempty(regexp(s,exReg,'once')),fs);
+    exInd=arrayfun(@(s)~isempty(regexp(s.name,exReg,'once')),fs);
 else
     exInd=false(size(fs));
 end
 fs=fs(ind&(~exInd));
-fds=fds(ind&(~exInd));
-fs=cellfun(@(p,f){fullfile(p,f)},fds,fs);
+% 加上路径
+fs=arrayfun(@(f){fullfile(f.folder,f.name)},fs);
 end
