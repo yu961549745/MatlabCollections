f588dba06df52faaedc59f1d62a8da6647f71430
鱼泡 <jiangtao.yjt@alibaba-inc.com>
2019-08-11 15:32:14 +0800

    重新整理sqlite测试案例

--- cell-sql/demo.m
+++ cell-sql/demo.m
@@ -1,12 +1,20 @@
 clc,clear,close all;
-javaaddpath('sqlite.jar');
+dbfile='tmp.db';
+if exist(dbfile,'file')
+    delete(dbfile)
+end
+conn = sqlite('tmp.db','create');
+conn.exec('create table test (name string, value double)');
 data={
-    'A','B';
     'a',1;
     'a',2;
     'b',1;
     'b',3;
     'c',4;
     'a',4};
-cells_sql(data,'cd','tmp','select A,max(B) from tmp group by A order by A')
-cells_sql(data,'cd','tmp','select B,A from tmp order by B,A')
+conn.insert('test',{'name','value'},data)
+res = conn.fetch('select name,sum(value) from test group by name');
+disp(res)
+delete(dbfile)
+conn.close()
+
